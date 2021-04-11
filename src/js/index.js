322ea0caef1057aab4ca77d10668858d1c62d106
Miguel Jeronimo <miguelangeljd93@gmail.com>
2021-04-11 14:14:42 -0500

    Agregamos la pantalla inicial con un spinner que cargara 3 segundos y despues cambiara a la siguiente ventana.

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+let cuerpo = document.querySelector('body');
+
+cuerpo.onload = function () {
+    //let a = Redireccionar
+    
+    setTimeout('Redireccionar();',3000);  
+    
+}
+
+function Redireccionar() {
+   window.location.href='blessings.html';
+}
