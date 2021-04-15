461bfb6b1e1bab180dde35806a6d33bdd5e454ea
Miguel Jeronimo <miguelangeljd93@gmail.com>
2021-04-14 23:49:14 -0500

    Consumimos la API de tibia para optener los mundos y llenar un select con esos datos

---
+++ src/js/mundo.js
@@ -0,0 +1,39 @@
+let calcular = document.getElementById('calcular');
+
+calcular.onclick = function (e) {
+    e.preventDefault();
+    obtenerMundos();
+}
+
+
+function obtenerMundos() {
+   let url = 'https://api.tibiadata.com/v2/worlds.json';
+   let array;
+   let worlds
+   fetch(url).
+   then(function(resp){return resp.json();}).
+   then(function(data){
+        try{
+            let selectWorlds = document.getElementById('selectWorlds');
+            let opciones = document.createElement('option');//crear un elemento
+            array = data.worlds.allworlds.length;
+            opciones.value="";
+            opciones.text="Seleccione una opción";
+            selectWorlds.appendChild(opciones);
+            for (let i = 0; i < array; i++) {
+                worlds = data.worlds.allworlds[i].name;
+                let opciones2 = document.createElement('option');
+                opciones2.value =worlds;
+                opciones2.text =worlds;
+                selectWorlds.appendChild(opciones2);
+            }
+        }
+        catch(error){
+            console.error(error);
+        }
+   }).catch(
+       function(error){
+           console.error(error.error());
+    });
+}
+
