838d03f3b04ea79c53781d9e2a4c734be1aa5c06
Miguel Jeronimo <mjeronimo@frd.org.mx>
2022-11-10 12:51:30 -0600

    Se actualizo el endpoint de los Mundos de tibia y se mostro cierta información

--- src/js/mundo.js
+++ src/js/mundo.js
@@ -7,32 +7,38 @@ cuerpo.onload  = function (){
 }
 
  function obtenerMundos(){
-    let url = 'https://api.tibiadata.com/v2/worlds.json';
+    let url = 'https://api.tibiadata.com/v3/worlds';
     let tabla = document.getElementById('tablas');
     let array;
     let worlds
-    let nombre, locacion, onlines, tipo_mundo, adicional;
+    let nombre, locacion, onlines, tipo_mundo;
+    let playerOnline, pvpType, premiumOnly, transferType, battleyeProtected;
+    let gameWorldType
     fetch(url).
     then(function(resp){return resp.json();}).
     then(function(data){
          try{
              let selectWorlds = document.getElementById('selectWorlds');
              let opciones = document.createElement('option');//crear un elemento
-             array = data.worlds.allworlds.length;
+             array = data.worlds.regular_worlds.length;
              for (let i = 0; i < array; i++) {
                 let datos = document.createElement('tr');
-                 nombre = data.worlds.allworlds[i].name;
-                 onlines = data.worlds.allworlds[i].online;
-                 locacion = data.worlds.allworlds[i].location;
-                 tipo_mundo = data.worlds.allworlds[i].worldtype;
-                 adicional = data.worlds.allworlds[i].additional;
+                 nombre = data.worlds.regular_worlds[i].name;
+                 onlines = data.worlds.regular_worlds[i].status;
+                 locacion = data.worlds.regular_worlds[i].location;
+                 tipo_mundo = data.worlds.regular_worlds[i].pvp_type;
+                 playerOnline = data.worlds.regular_worlds[i].players_online;
+                 premiumOnly = data.worlds.regular_worlds[i].premium_only;
+                 transferType = data.worlds.regular_worlds[i].transfer_type;
                  datos.innerHTML += `
                     <tr>
                     <td class="mdl-data-table__cell--non-numeric">${nombre}</td>
                     <td class="mdl-data-table__cell--non-numeric">${onlines}</td>
                     <td class="mdl-data-table__cell--non-numeric">${locacion}</td>
                     <td class="mdl-data-table__cell--non-numeric">${tipo_mundo}</td>
-                    <td class="mdl-data-table__cell--non-numeric">${adicional}</td>
+                    <td class="mdl-data-table__cell--non-numeric">${premiumOnly}</td>
+                    <td class="mdl-data-table__cell--non-numeric">${transferType}</td>
+                    <td class="mdl-data-table__cell--non-numeric">${playerOnline}</td>
                     </tr>
                  `;
                  tabla.appendChild(datos);
@@ -49,14 +55,14 @@ cuerpo.onload  = function (){
  }
 
  function playerOnline() {
-    url = `https://api.tibiadata.com/v2/worlds.json`;
+    url = `https://api.tibiadata.com/v3/worlds`;
     let personaje_online = document.getElementById('personaje_online');
     let onlines;
     fetch(url)
     .then((resp) => resp.json())
     .then(function (data) {
        try {
-        onlines = data.worlds.online;
+        onlines = data.worlds.players_online;
         personaje_online.innerText= 'Personajes online: '+onlines;
        } catch (error) {
            console.error(error);
